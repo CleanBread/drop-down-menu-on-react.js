a5cbff6856a29a224ae275ffe96e45d9cfbb2987
Gleb Chistyakov <gleb4ist@gmail.com>
2018-07-31 14:07:55 +0500

    drop

--- drop-menu/src/App.js
+++ drop-menu/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    isOpen: true
+  };
+
   render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
+    const div = this.state.isOpen && (
+      <div className="spisok">
+        <ul className="ul">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
       </div>
     );
+    return (
+      <div className="App">
+        <button className="button" onClick={this.HandOpen}>
+          Кнопка
+        </button>
+        {div}
+      </div>
+    );
+
+    HandOpen = () => {
+      console.log("---", "clicked");
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    };
   }
 }
 
