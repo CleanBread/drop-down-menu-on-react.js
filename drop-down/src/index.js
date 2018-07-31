9e660f0ed008dafdf7a6b4cd74d3a9d2f75516a8
Gleb Chistyakov <gleb4ist@gmail.com>
2018-07-31 11:29:42 +0500

    Hello

--- drop-down/src/index.js
+++ drop-down/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function Hello() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+render(<Hello />, document.getElementById("root"));
